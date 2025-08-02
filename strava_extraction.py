bd8498f4cdf57df54338982f06af01b039493147
Joaquim Francalanci <jofrancalanci@gmail.com>
2025-08-02 11:58:11 +0200

    Basic version

---
+++ strava_extraction.py
@@ -0,0 +1,15 @@
+from fitparse import FitFile
+import pandas as pd
+
+fitfile = FitFile("Brugai_Monte_Parè.fit")
+
+data = []
+
+for record in fitfile.get_messages('record'):
+    record_data = {}
+    for field in record:
+        record_data[field.name] = field.value
+    data.append(record_data)
+
+df_fit = pd.DataFrame(data)
+print(df_fit.head())
